074f5609892d8a7f6bf5890661245e7bb241f062
sonnguyen0627 <125434822+sonnguyen0627@users.noreply.github.com>
2024-07-10 06:24:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  introTitle: "Chị Bông à!",
+  introDesc: `Chị Bông trả lời giúp Topi một câu hỏi nhá`,
+  btnIntro: "Dạ",
+  title: "Phải chăng chị Bông đã gặp phải nhiều vấn đề cuộc sống !!!!!!!",
+  desc: "Vậy chị Bông chắc ăn phải giải quyết nó nhá ???",
+  btnYes: "Dạ chắc chắn !!!!!",
+  btnNo: "Hông giải quyết",
+  question:
+    "Vậy chị hứa rồi nhá , có đôi lời gì chị muốn gửi cho Topi hong !!!!!!",
+  btnReply: "Gửi cho Topi <3",
+  reply: "Chị Bông hứa không ăn hiếp Topi nữa , iu iu :333",
+  mess: "Chị lo mà ăn uống ngủ nghỉ đầy đủ đi nhóa, để có sức mà đi làm !!!",
+  messDesc:
+    "Buổi sáng vui vẻ nhá chị Bông, chị đi làm nhớ rút đơn xin nghỉ việc đó!!!! ",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/milkita.milkita202/about",
+};
